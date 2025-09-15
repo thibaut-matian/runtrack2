d18f62daaf05fa7afa69a71305c5a5f031351c4d
Theubald-bee <thibaut.matian@gmail.com>
2025-09-15 16:20:24 +0200

    deux premiers exercices

---
+++ jour01/job01/index.php
@@ -0,0 +1,17 @@
+<?php
+$str = "LaPlateforme";
+echo "$str";
+echo "<br>";
+
+$str2 = "Vive";
+$str3 = "!";
+
+echo "$str2"."$str"."$str3";
+echo "<br>";
+
+$val= 6;
+echo "$val";
+
+echo "<br>";
+$val +=4;
+echo "$val";
