19c4a9dcfacf965dd0dc86879c2a66e607decb73
Theubald-bee <thibaut.matian@gmail.com>
2025-09-18 09:54:14 +0200

    job04 jour 4 done

---
+++ jour04/job05/index.php
@@ -0,0 +1,19 @@
+<?php
+
+if (!empty($_POST)) {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+    if ($username === "John" && $password === "Rambo"){
+        echo "C'est pas ma guerre";
+    } else {
+        echo "Votre pire cauchemar";
+    }
+}
+
+?>
+
+<form method="post">
+    <input type="text" name="username" placeholder="Username">
+    <input type="text" name="password" placeholder="password" required />
+    <input type="submit" value="Envoyer">
+</form>
