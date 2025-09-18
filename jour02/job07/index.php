3cd731bde8db0e2c572acf43da1a40abf5766dd7
Theubald-bee <thibaut.matian@gmail.com>
2025-09-18 16:17:14 +0200

    job07 jour 2 tres dur

--- jour02/job07/index.php
+++ jour02/job07/index.php
@@ -1,5 +1,35 @@
 <?php
+<?php
+// $outerSpace = '';
+// $innerSpace = '';
+$LeftChar = '/';
+$RightChar = '\\';
+$Space = '  ';   // entités avec ';'
+$Height = 8;
 
-$hauteur=5;
+// init (nécessaires car variables commentées)
+$innerSpace = '';
+$foot = '';
+$CountA = 0;
+$CountB = 0;
 
-for ($i=0;$i<$hauteur;$i++)
+for ($CountA; $CountA < $Height; $CountA++) {
+    // (re)crée outerSpace à chaque tour
+    $outerSpace = '';
+    for ($countOuterSpaces = 0; $countOuterSpaces < ($Height - 1 - $CountA); $countOuterSpaces++) {
+        $outerSpace .= $Space;
+    }
+
+    echo $outerSpace . " /" . $innerSpace . "\\" . "<br />";
+    $innerSpace .= $Space . $Space;
+}
+for ($CountB; $CountB < $Height; $CountB++) {
+    $foot .= '---';
+}
+echo $foot . "<br />";
+?>
+
+
+
+</body>
+</html>
