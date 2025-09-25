b8b22037eb2f156e5cb6fa8271d444f78347b446
Theubald-bee <thibaut.matian@gmail.com>
2025-09-25 16:52:24 +0200

    job06 a job12

--- jour09/job02/job02.sql
+++ jour09/job02/job02.sql
@@ -11,12 +11,11 @@ INSERT INTO etages (id,nom, numero, superficie) VALUES
 (1,'RDC', 0, 500),
 (2,'R+1', 1, 500);
 
-INSERT INTO salles (id, nom, id_etage, superficie, nb_places) VALUES
+INSERT INTO salles (id, nom, id_etage, capacite) VALUES
 (1,'Lounge', 1, 100),
 (2,'Studio Son', 1, 5),
 (3,'Broadcasting', 2, 50),
 (4,'Bocal Peda', 2, 4),
 (5,'Coworking', 2, 80),
 (6,'Studio Video', 2, 5);
-
 
---
+++ jour09/job07/job07.sql
@@ -0,0 +1,1 @@
+SELECT * FROM etudiants WHERE naissance <= DATE_SUB(CURDATE(), INTERVAL 18 YEAR);
