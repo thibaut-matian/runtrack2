743ccecb4b67f017869c374e4f6b7e77f9752076
Theubald-bee <thibaut.matian@gmail.com>
2025-09-26 16:53:28 +0200

    jour 10 facile

---
+++ jour10/job03/index.php
@@ -0,0 +1,44 @@
+<?php
+// Connexion à la base de données
+$serveur = "localhost";
+$utilisateur = "root";
+$motdepasse = "";
+$basededonnees = "jour09";
+
+$connexion = new mysqli($serveur, $utilisateur, $motdepasse, $basededonnees);
+
+// Vérifier la connexion
+if ($connexion->connect_error) {
+    die("Connexion échouée: " . $connexion->connect_error);
+}
+// Requête SQL pour récupérer tous les étudiants
+$requete = "SELECT prenom, nom, naissance FROM  etudiants WHERE sexe = 'Femme';";
+$resultat = $connexion->query($requete);
+
+
+echo "<table border='1'>";
+echo "<thead>";
+echo "<tr>";
+echo "<th>Nom</th>";
+echo "<th>Capacité</th>";
+echo "<th>Naissance</th>";
+echo "</tr>";
+echo "</thead>";
+echo "<tbody>";
+
+if ($resultat->num_rows > 0) {
+    while ($ligne = $resultat->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $ligne['prenom'] . "</td>";
+        echo "<td>" . $ligne['nom'] . "</td>";
+        echo "<td>" . $ligne['naissance'] . "</td>";
+        echo "</tr>";
+    }
+} else {
+    echo "<tr><td colspan='6'>Aucun étudiant trouvé</td></tr>";
+}
+
+echo "</tbody>";
+echo "</table>";
+
+?>
